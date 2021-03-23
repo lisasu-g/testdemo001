e62fa8d212f2c1d316567724dbcb50ec080d33eb
Django Lee <django.lee@icloud.com>
2021-03-23 11:08:34 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,6 @@
-var cheerio = require("cheerio");
-var phantom = require("phantom");
 var moment = require("moment");
+var { parse } = require("node-html-parser");
 const puppeteer = require("puppeteer");
-
-
 const { DAILY_URL, SEND_MAIL, SEND_PASSWORD, TO_MAIL } = process.env;
 const reportPath = "./output/report.html";
 const send = require("gmail-send")({
@@ -21,31 +18,33 @@ const send = require("gmail-send")({
 const fs = require("fs");
 
 async function news() {
-  
   console.log(moment());
   console.log(moment().weekday());
   // 如果是周日，周六就啥也不干
   if (moment().weekday() === 6 || moment().weekday() === 0) {
-    console.log('休息日')
+    console.log("休息日");
     return;
   }
   // 交易日
-  console.log('交易日')
-  
-  
+  console.log("交易日");
+
   fs.writeFileSync(reportPath, "", "utf8");
-  
+
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.goto(DAILY_URL);
   const contentMain = await page.content();
+
+
   if (contentMain) {
-    fs.writeFileSync(reportPath, contentMain, "utf8");
+    const root = parse(contentMain);
+    const emailContent = root.querySelector('.pageContent').toString();
+    fs.writeFileSync(reportPath, emailContent, "utf8");
   } else {
     console.log("content not exist");
   }
   await browser.close();
-  
+
   await send();
 }
 
